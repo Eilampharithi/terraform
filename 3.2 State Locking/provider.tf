ca1f1564e882a32893b27e7adf16773187b60d54
Eilampharithi <eilampharithiarunachalam@gmail.com>
2025-05-09 21:34:04 +0530

    Update provider.tf

--- 3.2 State Locking/provider.tf
+++ 3.2 State Locking/provider.tf
@@ -8,8 +8,8 @@ terraform {
   }
   # Adding Backend as S3 for Remote State Storage
   backend "s3" {
-    bucket = "test-terra-2806"
-    key    = "dev/terraform.tfstate"
+    bucket = "test-terra-2806" # this should be available in the aws console
+    key    = "dev/terraform.tfstate" # this will create a folder called deva and store in it, in the name terraform.tfstate
     region = "ap-south-1" 
     # For State Locking, LockID
     dynamodb_table = "test-locking"    
